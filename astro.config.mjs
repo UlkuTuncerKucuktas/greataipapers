a0836d60509026ef350c74a06e3f3195f56007ad
UlkuTuncerKucuktas <ulku.tuncer.kucuktas@gmail.com>
2026-04-06 10:02:54 +0300

    Fix site URL and image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://greataipapers.com',
+    integrations: [mdx(), sitemap()],
+    image: {
+        service: { entrypoint: 'astro/assets/services/noop' },
+    },
 });
